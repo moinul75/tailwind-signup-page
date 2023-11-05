32f7d3029dcd30e1f286557b03334d410073eeba
moinul75 <freelancerudoy752@gmail.com>
2023-11-06 01:21:37 +0600

    implement signup page design

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './js/**/*.js',  
+    './css/**/*.css'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
